2e581137ea4093b60c29c587618dcc3456917110
jfreek <jorgee.abud@gmail.com>
2017-07-25 14:48:51 -0600

    comments & explanations

--- face_detection.py
+++ face_detection.py
@@ -12,17 +12,19 @@ cap = cv2.VideoCapture(0)
 
 while 1:
     ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
+    # scaleFactor – Parameter specifying how much the image size is reduced at each image scale.
+	# minNeighbors – Parameter specifying how many neighbors each candidate rectangle should have to retain it.
+    faces = face_cascade.detectMultiScale(image=gray, scaleFactor=1.3, minNeighbors=5)
 
     for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.rectangle(img=img, pt1=(x,y), pt2=(x+w,y+h), color=(255,0,0), thickness=2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
         
-        eyes = eye_cascade.detectMultiScale(roi_gray)
+        eyes = eye_cascade.detectMultiScale(image=roi_gray)
         for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            cv2.rectangle(img=roi_color, pt1=(ex,ey), pt2=(ex+ew,ey+eh), color=(0,255,0), thickness=2)
 
     cv2.imshow('img',img)
     k = cv2.waitKey(30) & 0xff
