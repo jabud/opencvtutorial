94a6676352a7fde3da1b2fbcee247f4a2de9cb5f
jfreek <jorgee.abud@gmail.com>
2017-07-25 18:41:20 -0600

    imread params

--- draw_image.py
+++ draw_image.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('images/jorge.jpg', 1)
+img = cv2.imread(filename='images/jorge.jpg', iscolor=1)
 
 # DRAW
 # line         START      END       B,G,R   WIDTH
